763d5109985aeb3c3d9b44dfe427ff3bb09d9313
vikas2489 <vk55002678@gmail.com>
2022-04-25 00:39:42 +0530

    not completed

--- block-BJaaih/code/index.js
+++ block-BJaaih/code/index.js
@@ -5,21 +5,29 @@
 
 // myMap function goes here
 
+function myMap(array) {
+    let final = [];
+    for (let i = 0; i < array.length; i++) {
+        final.push(array[i]);
+    }
+    return final;
+}
+
+Array.prototype.myMap = myMap;
+
 // Test the myMap function you created above
 
 let numbers = [1, 5, 6, 8, 9];
-let words = 'quick brown fox jumped over a lazy dog'.split(
-  ' '
-);
-let doubleNum = numbers.myMap(function (num) {
-  return num * 2 - 1;
+let words = 'quick brown fox jumped over a lazy dog'.split(' ');
+let doubleNum = numbers.myMap(function(num) {
+    return num * 2 - 1;
 });
 
 let capitalWords = words
-  .myMap(function (word) {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  })
-  .join(' ');
+    .myMap(function(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(' ');
 console.log(doubleNum); // it should be [1, 9, 11, 15, 17]
 console.log(capitalWords); // it should be 'Quick Brown Fox Jumped Over A Lazy Dog'
 
@@ -30,15 +38,15 @@ After adding the function test it using the code below.
 
 // You code goes here
 
-let even = numbers.myFilter(function (num) {
-  return num % 2 === 0;
+let even = numbers.myFilter(function(num) {
+    return num % 2 === 0;
 });
 
 let filteredWords = words
-  .myFilter(function (word) {
-    return word.length > 3;
-  })
-  .join(' ');
+    .myFilter(function(word) {
+        return word.length > 3;
+    })
+    .join(' ');
 console.log(even); // it should be [6, 8]
 console.log(filteredWords); // it should be 'quick brown jumped over lazy';
 
